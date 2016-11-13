eebcd1375a1a3d6a686c133f8d14f85686bfdd01
ajakaiye33 <ajakzhedgar@gmail.com>
2016-11-13 21:39:56 +0100

    the Hello App function

---
+++ helloworldapp.py
@@ -0,0 +1,8 @@
+print('--------------------------------')
+print('          HELLO APP')
+print('--------------------------------')
+print()
+def greetings():
+    askName = input('Hey, what is your name? >')
+    sayHello = print('Hi ' + askName + ' welcome!!')
+greetings()
