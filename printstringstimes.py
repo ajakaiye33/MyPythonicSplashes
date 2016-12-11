a0773a8beeb43b4a0dcf537b708a24af2277b86d
ajakaiye33 <ajakzhedgar@gmail.com>
2016-12-11 12:48:17 +0100

    print strings by the number

---
+++ printstringstimes.py
@@ -0,0 +1,7 @@
+#a function that takes two arguements(string and number) and output the string by the number indicated
+def string_times(strr, num):
+    #return strr * num
+    print(strr * num)
+
+string_times("'cos I'm happy!\n", 5)
+
